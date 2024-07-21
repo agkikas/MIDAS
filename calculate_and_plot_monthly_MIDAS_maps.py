079b404bbda88d889ed9366bebda53b2bf9e9c03
Antonis Gkikas <agkikas@noa.gr>
2024-07-21 17:40:08 +0300

    First complete run

--- calculate_and_plot_monthly_MIDAS_maps.py
+++ calculate_and_plot_monthly_MIDAS_maps.py
@@ -145,7 +145,8 @@ regmidasfiles=sorted(glob.glob(regncfolder + '/**/*.nc', recursive=True))
 
 # %%
 #Read all files at once!!!
-combncs=xr.open_mfdataset(regmidasfiles,parallel=True,coords='minimal',data_vars='minimal',compat='override',concat_dim='Time',combine='nested', name)
+#combncs=xr.open_mfdataset(regmidasfiles,parallel=True,coords='minimal',data_vars='minimal',compat='override',concat_dim='Time',combine='nested')
+combncs=xr.open_mfdataset(regmidasfiles,parallel=True)
 
 # %%
 #Select only the MIDAS DOD
@@ -155,5 +156,89 @@ midasdod = combncs['Modis-total-dust-optical-depth-at-550nm']
 #Calculate the monthly means
 regdommmean = midasdod.resample(Time='1MS').mean(dim='Time')
 
+# %%
+#Create an array with the indices of the months of the year
+monidxs=np.arange(1,13)
+monnames=['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
 
+for monidx,monname in zip(monidxs,monnames):
+    
+    print (monname,monidx)
+
+    #Select each month for the regional domain (MIDAS DOD)
+    monregdommmean=regdommmean.sel(Time=regdommmean.indexes['Time'].to_datetimeindex().month==monidx)
+
+    ###################################################################################
+    #Plot the monthly maps of the MIDAS DOD
+    #Map projection
+    mapproj=ccrs.PlateCarree()
+
+    fig = plt.figure(figsize=(figx,figy))
+
+    ax = fig.add_subplot(1,1,1,projection=mapproj,facecolor='darkgrey')
+
+    cbar_ax = fig.add_axes([0, 0, 0.1, 0.1])
+
+    ax.set_extent([westlon, eastlon, southlat, northlat], crs=ccrs.PlateCarree())
+
+    posn = ax.get_position()
+
+    cf=ax.pcolormesh(monregdommmean.Longitude.values,monregdommmean.Latitude.values,monregdommmean.values,
+                            transform=ccrs.PlateCarree(),cmap=cmap,norm=norm)
+
+    #cf.cmap.set_under(cbarunder)
+    cf.cmap.set_over(cbarover)
+
+    # colorbar and labels
+    cb = plt.colorbar(cf,cmap=cmap,spacing='proportional',norm=norm,ticks=bounds,
+                    boundaries=bounds,cax=cbar_ax,extend=cbarextend)
+
+    cb.ax.set_ylabel(cbartext,fontsize=cbarlbsize,labelpad=cbarlbpad,fontweight=cbarlbweight,rotation=270) 
+    cb.ax.set_position([posn.x0 + posn.width + 0.05, posn.y0,0.04, posn.height])
+
+    titletext = satellite.replace('MODIS','MIDAS')+'  '+str(monidx).zfill+str(monregdommmean.indexes['Time'].to_datetimeindex().year[0])
+
+    ax.set_title(titletext,fontweight=titletextfw,fontsize=titletextfs,y=titletexty)
+
+    for t in cb.ax.get_yticklabels():
+                
+        t.set_fontsize(cbartickssize)
+        t.set_fontweight(cbarticksfont)
+        t.set_horizontalalignment('right')   
+        t.set_x(xticksdist)
+
+    gl = ax.gridlines(draw_labels=True, crs=ccrs.PlateCarree(), lw=0.75,ls='--',color="white",
+                    y_inline=False, xlocs=range(westlon,eastlon,merstep), ylocs=range(southlat,northlat,parstep))
+
+    gl.xformatter = LONGITUDE_FORMATTER
+    gl.yformatter = LATITUDE_FORMATTER
+    gl.xlabel_style = {'color': 'black', 'weight': 'bold', 'size': grlblfsize}
+    gl.ylabel_style = {'color': 'black', 'weight': 'bold', 'size': grlblfsize}
+
+    ax.coastlines(linewidth=0.5,color='k')
+
+    resol = '10m'  # use data at this scale
+    bodr = cfeature.NaturalEarthFeature(category='cultural', 
+        name='admin_0_boundary_lines_land', scale=resol, facecolor='none')
+    # land = cfeature.NaturalEarthFeature('physical', 'land', \
+    #     scale=resol, edgecolor='k', facecolor=cfeature.COLORS['land'])
+    # ocean = cfeature.NaturalEarthFeature('physical', 'ocean', \
+    #     scale=resol, edgecolor='none', facecolor=cfeature.COLORS['water'])
+    # lakes = cfeature.NaturalEarthFeature('physical', 'lakes', \
+    #     scale=resol, edgecolor='b', facecolor=cfeature.COLORS['water'])
+
+    #ax.add_feature(land, facecolor='grey')
+    #ax.add_feature(ocean,facecolor='Gainsboro')
+    #ax.add_feature(lakes)
+    ax.add_feature(bodr, linestyle='--', edgecolor='k', alpha=1)
+
+    outputfilename = satellite.replace('MODIS','MIDAS')+'_'+studyregion+'_DOD550_'+monname+' '+str(monregdommmean.indexes['Time'].to_datetimeindex().year[0])+'.png'
+
+    plt.savefig(os.path.join(outputfolder,outputfilename),dpi=600,bbox_inches='tight')
+
+    plt.show()
+
+    plt.close()
+
+    fig.clear()
 # %%
